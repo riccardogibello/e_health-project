b1c242a293e25263dc85f3c40d71e325d95f0714
Vadym Nahrudnyy <vadym.nahrudnyy@mail.polimi.it>
2021-12-21 22:19:34 +0100

    Final commit before delivery :)

--- main.py
+++ main.py
@@ -5,29 +5,6 @@ from multiprocessing.pool import ThreadPool
 from PyQt5.QtWidgets import QApplication
 from GUI.GUIManager import GUIManager
 from GUI.ProcessHandler import ProcessHandler
-from DataManagers.OldDatasetManager import *
-from DataManagers.DataMiner import DataMiner
-
-
-def launch_data_miner():
-    miner = DataMiner()
-    miner.fill_database()
-
-
-def launch_new_dataset_manager():
-    pass
-    # new_dataset_manager = DatasetManager(KAGGLE_DATASET_PATH)
-    # new_dataset_manager.load_apps_into_db()
-
-
-def launch_old_dataset_manager():
-    old_dataset_manager = OldDatasetManager()
-    old_dataset_manager.load_old_dataset_into_db()
-
-
-def launch_words_miner():
-    pass
-    # word_miner.find_serious_games_words()
 
 
 def create_process_manager_thread():
